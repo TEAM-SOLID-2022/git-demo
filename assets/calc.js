800f0d5014ed665e750c11d97ae9e714755a6bcc
NyxSalape <onyx.salape@gmail.com>
2022-10-03 21:56:07 +0800

    Separated script from calculator, assets calc.js

---
+++ assets/calc.js
@@ -0,0 +1,59 @@
+"use strict";
+let display = document.getElementById("result");
+let x = 0;
+let isOperateClick = false;
+let operation;
+
+function dsp(val) {
+  if (isOperateClick == true) {
+    display.value = "0";
+    isOperateClick = false;
+  }
+  if (display.value == "0") {
+    display.value = "";
+  }
+  if(val!="." || !(display.value.includes("."))){
+      display.value += val;
+  }
+  }
+
+function operate(sign) {
+  isOperateClick = true;
+  if (operation == "") {
+    operation = sign;
+  }
+  if (x != "") {
+    switch (operation) {
+      case "+":
+        x = parseFloat(display.value) + parseFloat(x);
+        break;
+      case "-":
+        x = parseFloat(x) - parseFloat(display.value);
+        break;
+      case "x":
+        x = parseFloat(x) * parseFloat(display.value);
+        break;
+      case "÷":
+        x = parseFloat(x) / parseFloat(display.value);
+        break;
+    }
+  } else if (display.value != "") {
+    x = display.value;
+  }
+  console.log(operation);
+  console.log(x);
+  operation = sign;
+  display.value = x;
+}
+
+function calc() {
+  operate(operation);
+  operation = "";
+  x = "";
+}
+
+function clr() {
+  display.value = "";
+  x = "";
+  operation = "";
+}
