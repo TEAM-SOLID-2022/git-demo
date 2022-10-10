a2dfd2ee64839f015ab2c3b369df5e221666d8cb
wacanam <wave.ambray@benmanage.com>
2022-10-10 13:36:33 +0800

    class auth

--- assests/js/auth.js
+++ assests/js/auth.js
@@ -3,19 +3,27 @@ import { Authentication } from "./Authentication.class.js";
 const Auth = new Authentication();
 
 const formElement = document.getElementById("login-form");
+const logout_btn = document.getElementById("logout_btn");
 
+logout_btn.addEventListener("click", logout);
 formElement.addEventListener("submit", login);
 
 function login(e) {
     e.preventDefault();
     const uname = formElement.uname.value;
     const pwd = formElement.pwd.value;
-    const auth_status = Auth.login(uname, pwd);
-    console.log(auth_status)
+    Auth.login(uname, pwd);
+    if (Auth._auth_status) {
+        logout_btn.style.display = "block";
+        formElement.login.style.display = "none";
+    }
+    console.log("Authenticated", Auth._auth_status)
 }
 
 function logout() {
-    console.log("logout")
+    Auth.logout();
+    console.log("Authenticated", Auth._auth_status)
+
 }
 
 
